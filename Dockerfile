d55e8c15afa8ca75f5c66a1b5e4eb80bf2bc8029
Guillermo <ggjnez92@gmail.com>
2021-11-23 00:06:46 +0100

    Update docker configuration

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,24 @@
-FROM node:14.18.0-alpine3.11
+FROM node:lts
 
-RUN apk update && apk upgrade && \
-    apk add --no-cache bash git openssh
+RUN apt update && \
+    apt install -y bash nano
 
-RUN mkdir -p /usr/src/app && \
-    chown -R node:node /usr/src/app
+RUN mkdir -p /usr/src/app \
+    && mkdir -p /data
 WORKDIR /usr/src/app
-
-ARG NODE_ENV
-ENV NODE_ENV $NODE_ENV
-
-RUN git -C /usr/src clone https://github.com/binary-coffee-dev/nodebb-theme-binarycoffee.git
-RUN cd /usr/src/nodebb-theme-binarycoffee && npm install
-
-COPY --chown=node:node install/package.json /usr/src/app/package.json
-
-USER node
-
-RUN npm install --only=prod && \
-    npm cache clean --force
-
-RUN ln -s /usr/src/nodebb-theme-binarycoffee /usr/src/app/node_modules/nodebb-theme-binarycoffee
-
-COPY --chown=node:node . /usr/src/app
 
 ENV NODE_ENV=production \
     daemon=false \
     silent=false
 
-CMD node ./nodebb build ;  node ./nodebb start
+COPY install/package.json /usr/src/app/package.json
+
+RUN npm install --only=prod \
+    && npm cache clean --force
+
+COPY . /usr/src/app
+
+RUN rm -f config.json \
+    && chmod +x init.sh
+
+ENTRYPOINT [ "./init.sh" ]
---
+++ init.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -e
+
+if [ -f .env ]
+then
+  export $(cat .env | xargs)
+fi
+
+CONFIG=$(cat <<-END
+{
+    "url": "$URL",
+    "database": "mongo",
+    "port": "$PORT",
+    "mongo:host": "$DB_HOST",
+    "mongo:port": "$DB_PORT",
+    "mongo:username": "$DB_USER",
+    "mongo:password": "$DB_PASSWORD",
+    "mongo:database": "$DB_NAME",
+    "admin:username": "$ADMIN_NAME",
+    "admin:password": "$ADMIN_PASSWORD",
+    "admin:password:confirm": "$ADMIN_PASSWORD",
+    "admin:email": "$ADMIN_EMAIL"
+}
+END
+)
+if ! [ -e /data/config.json ]; then
+
+  node app --setup=\"$(printf '%s' $(echo $CONFIG))\"
+
+  mv /usr/src/app/config.json /data/config.json
+  ln -s /data/config.json /usr/src/app/config.json
+
+else
+
+  if [ ! -e /usr/src/app/config.json ]; then
+    ln -s /data/config.json /usr/src/app/config.json
+  fi
+
+fi
+
+if [ ! -e /data/uploads ]; then
+    mv /usr/src/app/public/uploads /data/uploads \
+    && ln -s /data/uploads /usr/src/app/public/uploads
+else
+    rm -rf /usr/src/app/public/uploads \
+    && ln -s /data/uploads /usr/src/app/public/uploads
+fi
+
+if [ -f config.json ]; then
+  /usr/src/app/nodebb build --series
+  /usr/src/app/nodebb upgrade -mips
+fi
+
+node ./nodebb start
